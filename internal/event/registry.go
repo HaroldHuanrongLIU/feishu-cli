888b72d3258d79a559f9cb45252e22578f78fdfc
shanglei <shanglei.ops@bytedance.com>
2026-06-13 13:33:57 +0800

    feat: add SubscriptionType and SingleConsumer to EventKey definition

--- internal/event/registry.go
+++ internal/event/registry.go
@@ -26,6 +26,16 @@ func RegisterKey(def KeyDefinition) {
 		panic(fmt.Sprintf("EventKey %s: EventType must not be empty", def.Key))
 	}
 
+	switch def.SubscriptionType {
+	case "":
+		def.SubscriptionType = SubTypeEvent
+	case SubTypeEvent, SubTypeCallback:
+		// ok
+	default:
+		panic(fmt.Sprintf("EventKey %s: SubscriptionType must be %q or %q; got %q",
+			def.Key, SubTypeEvent, SubTypeCallback, def.SubscriptionType))
+	}
+
 	validateSchema(def)
 	validateParams(def)
 	validateAuth(def)
